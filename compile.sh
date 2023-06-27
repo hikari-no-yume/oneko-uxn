51791efe84bdfd110fb7bd85d8b9908af54042eb
hikari_no_yume <hikari@noyu.me>
2023-06-27 17:00:23 +0200

    Assemble resulting uxntal file, now that compiling works

--- compile.sh
+++ compile.sh
@@ -8,9 +8,15 @@ if [[ "x$CHIBICC_DIR" == "x" ]]; then
     CHIBICC_DIR=../chibicc
 fi
 
+if [[ "x$UXNASM" == "x" ]]; then
+    UXNASM=uxnasm
+fi
+
 set -e
 
 # preprocess
 "$CC" -I"$CHIBICC_DIR" -P -E -x c "./oneko-uxn/oneko.c" -o tmp.c
-# compile
-"$CHIBICC_DIR"/chibicc -O tmp.c
+# compile to uxntal
+"$CHIBICC_DIR"/chibicc -O tmp.c > tmp.tal
+# assemble uxntal
+$UXNASM tmp.tal oneko-uxn.rom
