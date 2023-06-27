e68378efa8ce3b0d63bd700ff304c77b6bef1909
hikari_no_yume <hikari@noyu.me>
2023-06-27 14:52:50 +0200

    Add compile script

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ "x$CC" == "x" ]]; then
+    CC=cc
+fi
+
+if [[ "x$CHIBICC_DIR" == "x" ]]; then
+    CHIBICC_DIR=../chibicc
+fi
+
+set -e
+
+# preprocess
+"$CC" -I"$CHIBICC_DIR" -I"./oneko-uxn/" -P -E -x c "./oneko-uxn/oneko.c" -o tmp.c 
+# compile
+"$CHIBICC_DIR"/chibicc -O tmp.c
